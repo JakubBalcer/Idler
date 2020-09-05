58d6ee087c8ac11688b5010c3a4801e924ca44a1
Jakub Balcer <UltimateDespair@localhost.localdomain>
2020-09-05 13:26:23 +0200

    buttons with text

--- Button.py
+++ Button.py
@@ -1,25 +1,54 @@
 import pygame
 
+# TO DO 
+# BOUND TEXT INSIDE BUTTON
+# CENTERING TEXT INSIDE BUTTON
+
 class Button:
-    def __init__(self, screen, x, y, size, shape):
+    def __init__(self, x, y, size, shape):
         self.pos = self.x, self.y = x, y
         self.size = self.width, self.height = size
         self.shape = shape
         self.visible = True
         self.destroyed = False
-        self.screen = screen
+        self.action = None
+        self.text = None
 
-    def show(self):
+    def show(self, surface):
         if self.visible:
-            self.establish()
+            self.establish(surface)
+            if self.text:
+                self.renderText(surface)
 
-    def establish(self):
+
+    def establish(self, surface):
         if self.shape == 'rect':
             self.area = pygame.Rect(self.pos,self.size)
-            pygame.draw.rect(self.screen, pygame.Color(255,255,255), self.area )
+            pygame.draw.rect(surface, pygame.Color(255,255,255), self.area )
 
     def clicked(self, pos):
         if self.shape != 'ellipse':
             if self.area.collidepoint(pos):
-                print("INSIDE")
+                self.dispatchAction()
+
+    def setAction(self, action):
+        self.action = action
+
+    def renderText(self, surface):
+        self.text.setPos(self.pos)
+        self.text.show(surface)
             
+    def dispatchAction(self, action=None):
+        try:
+            if self.action:
+                self.action()
+            elif action:
+                action()
+            else:
+                raise Exception("Missing action")
+        except:
+            print("EXCEPTION: Missing action")
+            
+    def setText(self, text):
+        self.text = text
+
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame,sys
 from Button import Button
+from Text import Text
+
+pygame.init()
 
 size = width, heigth = 480, 320
 screen = pygame.display.set_mode(size)
 paused = False
 
 buttons = []
-buttons.append(Button(screen, 20, 20, (20,20), 'rect'))
+b = Button(20, 20, (30,20), 'rect')
+b.setAction(lambda : print("Inside"))
+buttons.append(b)
+
+text = Text("TEST")
+text.setPos((20,40))
+text.color = (0,0,0)
+
+b.text = text
 
 while not paused:
     for event in pygame.event.get():
@@ -18,6 +29,7 @@ while not paused:
 
     
     screen.fill(pygame.Color(20,60,180))
+    text.show(screen)
     for btn in buttons:
-        btn.show()
+        btn.show(screen)
     pygame.display.flip()
